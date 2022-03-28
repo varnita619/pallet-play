a5ad63fb13b1bcab3fd90053ce1ee3853f027be4
Varnita Makrariya <crabthe64@gmail.com>
2022-03-28 19:30:22 +0530

    feat: added  homepage

---
+++ src/Components/Navigation/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import "../../CSS/Reset.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <header className="nav-bar">
+        <div className="nav-bar_row">
+          <div className="nav-bar_section--align-start">
+            <button className="nav-bar_action-item">
+              <i className="fas fa-bars"></i>
+            </button>
+            <span className="nav-bar_heading heading heading-font">Pallet </span>{" "}
+            <span className="sub-heading heading-font"> Play</span>
+          </div>
+
+          <div className="search">
+            <input type="text" placeholder="Search.." name="search" className="search-input" />
+            <button type="submit" className="search-btn">
+              <i className="fa fa-search"></i>
+            </button>
+          </div>
+
+          <div className="nav-bar_section--align-end">
+            <button className="login-btn btn">Login</button>
+            <button className="nav-bar_action-item">
+              <img
+                src="https://images.unsplash.com/photo-1543132220-3ec99c6094dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHByb2ZpbGV8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                className="round-avatar-image avatar-sm"
+              />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export { NavBar };
