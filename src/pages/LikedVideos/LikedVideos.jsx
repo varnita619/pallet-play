9cfbceb3671bd1bc982f138e072675d5fe110afe
Varnita Makrariya <crabthe64@gmail.com>
2022-04-02 16:46:02 +0530

    refactor: styled video cards

--- src/pages/LikedVideos/LikedVideos.jsx
+++ src/pages/LikedVideos/LikedVideos.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../LikedVideos/LikedVideos.css";
 import { MdDelete, MdWatchLater } from "react-icons/md";
 import { useLikedVideoContext } from "../../Context/LikedVideosContext";
+import { SideBar } from "../../Components/SideBar/SideBar";
 
 const LikedVideos = () => {
   const {
@@ -10,32 +11,38 @@ const LikedVideos = () => {
   } = useLikedVideoContext();
 
   return (
-    <div className="liked-videos-container">
-      {likes?.length === 0 ? (
-        <div className="starting-text">You haven't liked any video yet.</div>
-      ) : (
-        likes.map((eachVideo) => (
-          <div className="shadow-card" key={eachVideo._id}>
-            <div className="card-image">
-              <img src={eachVideo.thumbnail} alt={eachVideo.title} />
-            </div>
+    <div className="main-container">
+      <SideBar />
+      <div className="videos-wrapper">
+        {likes?.length === 0 ? (
+          <div className="starting-text">You haven't liked any video yet.</div>
+        ) : (
+          likes.map((eachVideo) => (
+            <div className="shadow-card" key={eachVideo._id}>
+              <div className="card-image">
+                <img src={eachVideo.thumbnail} alt={eachVideo.title} />
+              </div>
 
-            <div className="shadow-card-details">
-              <h4>{eachVideo.title}</h4>
-              <p>{eachVideo.description}</p>
-            </div>
+              <div className="shadow-card-details">
+                <h4>{eachVideo.title}</h4>
+                <p>{eachVideo.description}</p>
+              </div>
 
-            <div className="twin-btn">
-              <button className="like-btn" onClick={() => removeFromLikes(eachVideo._id)}>
-                <MdDelete />
-              </button>
-              <button className="like-btn">
-                <MdWatchLater />
-              </button>
+              <div className="twin-btn">
+                <button
+                  className="like-btn"
+                  onClick={() => removeFromLikes(eachVideo._id)}
+                >
+                  <MdDelete />
+                </button>
+                <button className="like-btn">
+                  <MdWatchLater />
+                </button>
+              </div>
             </div>
-          </div>
-        ))
-      )}
+          ))
+        )}
+      </div>
     </div>
   );
 };
