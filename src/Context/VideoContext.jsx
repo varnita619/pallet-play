800a3ef51fe2d1d89048a182eccd32a969d802a5
Varnita Makrariya <crabthe64@gmail.com>
2022-04-06 01:32:25 +0530

    feat: added filter by category

--- src/Context/VideoContext.jsx
+++ src/Context/VideoContext.jsx
@@ -8,16 +8,22 @@ import {
 import axios from "axios";
 import { videoReducer } from "../Reducer/videoReducer";
 import { toast } from "react-hot-toast";
+import { uniqueCategory } from "../Utils/getUniqueCategory";
+import { filterByCategory } from "../Utils/filterByCategory";
 
 const VideoContext = createContext();
 
 const initialState = {
   videos: [],
+  categoryName: "ALL",
 };
 
 const VideoContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(videoReducer, initialState);
-  const { videos } = state;
+  const { videos, categoryName } = state;
+  const getUniqueCategory = uniqueCategory(videos, "categoryName");
+  const getFilterByCategory = filterByCategory(videos, categoryName);
+  console.log(getFilterByCategory)
 
   // for toggling sidebar
   const [active, setActive] = useState(false);
@@ -42,7 +48,15 @@ const VideoContextProvider = ({ children }) => {
 
   return (
     <VideoContext.Provider
-      value={{ state, dispatch, videos, active, setActive }}
+      value={{
+        state,
+        dispatch,
+        videos,
+        active,
+        setActive,
+        getUniqueCategory,
+        getFilterByCategory,
+      }}
     >
       {children}
     </VideoContext.Provider>
